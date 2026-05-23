3675a7e7afd34eda11e8785220c24e725dde137f
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-05-23 22:31:37 +0600

    Fixed: Global Handler error

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,5 +1,4 @@
 import type { NextFunction, Request, Response } from "express";
-import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
 const globalErrorHandler = (
   err: Error,
@@ -7,22 +6,6 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  if (err instanceof TokenExpiredError) {
-    return res.status(401).json({
-      success: false,
-      message: "Token expired",
-      errors: "Token expired",
-    });
-  }
-
-  if (err instanceof JsonWebTokenError) {
-    return res.status(401).json({
-      success: false,
-      message: "Invalid token",
-      errors: "Invalid token",
-    });
-  }
-
   res.status(500).json({
     success: false,
     message: err.message || "Internal Server Error",
